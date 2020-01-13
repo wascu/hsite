808512924e2a953bb1ce0ad81cf44fe263344a72
wascu <wasku@qq.com>
2020-01-13 15:15:02 +0800

    update ignore and support edit patients info

--- home/login/models.py
+++ home/login/models.py
@@ -1,20 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+
 class User(models.Model):
-    gender=(
-        ('male','男'),
-        ('female','女')
+    gender = (
+        ('male', '男'),
+        ('female', '女')
     )
 
-    name = models.CharField(max_length=128,unique=True)
+    name = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=32,choices=gender,default='男')
+    sex = models.CharField(max_length=32, choices=gender, default='男')
     c_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Patient(models.Model):
+    p_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, unique=False)
+    gender = (
+        ('male', '男'),
+        ('female', '女')
+    )
+    sex = models.CharField(max_length=32, choices=gender, default='男')
+    c_time = models.DateTimeField(auto_now_add=True)
+    history = models.TextField()
+    solution = models.TextField()
+
+    def __str__(self):
+        return "{0},{1}".format(self.p_id, self.name)
+
+
 
 class Meta:
     ordering = ['c_time']
